f9135c03a4570cb1a296b943c8bdfaab45a30c1f
rachelemmer <rachelaemmer@gmail.com>
2020-05-19 14:17:05 -0600

    create dashboard component

--- src/ViewAllHikes.js
+++ src/ViewAllHikes.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react'
 import { Map, Marker, Popup, TileLayer } from "react-leaflet";
-import { Icon } from "leaflet";
 
 export default class ViewAllHikes extends Component {
     
