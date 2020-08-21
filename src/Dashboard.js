4ca6efbe2de22b7f98cda7323a6614274b5d1a17
rachelemmer <rachelaemmer@gmail.com>
2020-08-20 21:11:20 -0600

    fix css for log out button

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -95,7 +95,7 @@ export default class Dashboard extends Component {
                         </div>
                     </Popup>
                     )}
-                    )}
+                    )
                 </Map> 
                 </div>    
             </div>
